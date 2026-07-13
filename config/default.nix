b6b169135f7911f84836d9e8841c7bfec2e73619
khoi <6994441+khoi@users.noreply.github.com>
2026-07-13 23:23:36 +0100

    firmware: expose split battery and enable deep sleep
    Keep the auxiliary battery service central-only while sharing the
    30-minute battery power-off policy across both halves.

--- config/default.nix
+++ config/default.nix
@@ -4,6 +4,6 @@
 
 let
   config = ./.;
-  go60_left = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
+  go60_left = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf;${config}/go60_lh.conf"; };
   go60_right = firmware.zmk.override { board = "go60_rh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60.conf"; };
 in firmware.combine_uf2 go60_left go60_right "go60"
